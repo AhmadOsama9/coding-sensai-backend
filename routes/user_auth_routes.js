cecc336bbf25d4b1e10730210c89f102e30dac4c
AhmedOsama <ahmedosamaa975@gmail.com>
2024-10-24 17:11:11 +0300

    Update user_auth_routes.js

--- routes/user_auth_routes.js
+++ routes/user_auth_routes.js
@@ -1,30 +1,32 @@
-const express = require("express");
 const passport = require("passport");
-
+const express = require("express");
 const router = express.Router();
-
-const user_auth_controller = require("../controllers/user_auth_controller")
+const user_auth_controller = require("../controllers/user_auth_controller");
 
 // Use href to go to the google auth page
 // I think we could just handle it from the frontend
 // By calling the api/user/auth/google
 
-
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
-
-router.get("/google/callback", passport.authenticate("google", { 
-    session: false, }),
+router.get("/google/callback", 
+    passport.authenticate("google", { session: false }),
+    (req, res, next) => {
+        console.log("Google callback route called");
+        next();
+    },
     user_auth_controller.auth_callback
 );
-
 
 router.get("/github", passport.authenticate("github", { scope: ["read:user", "user:email"] }));
 
-router.get("/github/callback", passport.authenticate("github", { 
-    session: false, }),
+router.get("/github/callback", 
+    passport.authenticate("github", { session: false }),
+    (req, res, next) => {
+        console.log("GitHub callback route called");
+        next();
+    },
     user_auth_controller.auth_callback
 );
-
 
 module.exports = router;
